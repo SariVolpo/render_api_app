2c41f8c10813bab7782a465bf5f915fd317ebcf8
Sari Volpo <v0773500276@gmail.com>
2026-04-14 13:24:37 +0300

    update api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const sdk = require('api')('@render-api/v1.0#dnv3m1pl9q6m88');
+// const sdk = require('api')('@render-api/v1.0#dnv3m1pl9q6m88');
+const sdk = require('api')('@render-api/v1.0#76e6p31pkyv61v2');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
