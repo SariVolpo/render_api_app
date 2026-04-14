4961f300f178198d2cfdf5422bfcf623b44e1d71
Sari Volpo <v0773500276@gmail.com>
2026-04-14 13:04:53 +0300

    change api key

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const sdk = require('api')('@render-api/v1.0#dnv3m1pl9q6m88');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// הגדרת ה-API Key לשימוש ב-SDK
-sdk.auth('YOUR_RENDER_API_KEY');
+// במקום לכתוב את המפתח ישירות, אנחנו קוראים אותו ממשתני הסביבה
+sdk.auth(process.env.RENDER_API_KEY);
 
 app.get('/apps', async (req, res) => {
   try {
